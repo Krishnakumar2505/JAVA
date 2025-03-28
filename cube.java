52dcf456340199ce2309bbcd8e76c2a8b024bd25
Krishnakumar2505 <krishna2522kumar@gmail.com>
2025-03-28 20:58:33 +0530

    java file

---
+++ cube.java
@@ -0,0 +1,9 @@
+public class cube {
+    public static void main(String[] args) {
+        for(int i=1;i<=7;i++){
+            int c=i*i*i;
+            System.out.println(c);
+        }
+    }
+    
+}
---
+++ sequence.java
@@ -0,0 +1,9 @@
+public class sequence {
+    public static void main(String[] args) {
+        for(int i=2;i<=4;i++){
+            int c=(int) Math.pow(i,i);
+            System.out.println(c);
+        }
+    }
+    
+}
